0c0b9c4fc6ea0d88045871d18cb6237a35b99a2f
Thai <ThaiDuongPhamLe02@gmail.com>
2023-12-17 23:36:49 +0100

    implement get route for tasks

--- backend/api.go
+++ backend/api.go
@@ -1,1 +1,22 @@
-package TODO_List
+package backend
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func InitServer() {
+	router := gin.Default()
+	router.GET("user/tasks", getTasks)
+
+	router.Run("localhost:8080")
+}
+
+func getTasks(ctx *gin.Context) {
+	//user := ctx.Param(":user")
+	ctx.IndentedJSON(http.StatusOK, SelectUserTasks(1))
+}
+
+func postTasks(ctx *gin.Context) {
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
-package TODO_List
+package main
+
+import (
+	"github.com/Thaaaii/TODO-List/backend"
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
+	//backend.CreateTableUsers()
+	//backend.CreateTableTasks()
+	//backend.CreateTableCategories()
+	//
+	//lastUserID, _ := backend.InsertUserIntoTable("user1", "passwort")
+	//lastTaskID, _ := backend.InsertTaskIntoTable("Sachen kaufen", "Weihnachtsgeschenke für Schwester", false, lastUserID)
+	//backend.InsertCategoryIntoTable("Freizeit", lastTaskID)
+	//
+	//lastUserID, _ = backend.InsertUserIntoTable("user2", "admin")
+	//lastTaskID, _ = backend.InsertTaskIntoTable("TODO-Listen App", "Frontend erstellen", true, lastUserID)
+	//_, _ = backend.InsertCategoryIntoTable("Studium", lastTaskID)
 
+	backend.InitServer()
 }
